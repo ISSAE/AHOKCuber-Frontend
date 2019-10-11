90e5fb1746adf9f26b067db8e31d1ebf17762d3e
Adnan Hamzeh <adnan.37h@gmail.com>
2019-10-11 23:27:04 +0300

    Add main, login and register screens

---
+++ src/views/Login.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login-screen">
+    <div class="header">
+      <router-link to="/">
+        <i class="fa fa-arrow-left fa-2x"></i>
+      </router-link>
+    </div>
+    <div class="body">
+      <div>
+        <i class="fa fa-4x fa-car"></i>
+        <h1>CUBER</h1>
+      </div>
+
+      <div style="width: 100%">
+        <input class="form-control mb-3" type="text" placeholder="Addresse email" />
+        <input class="form-control mb-3" type="text" placeholder="Mot de passe" />
+        <button class="btn btn-block btn-primary">Se connecter</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.login-screen {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+.login-screen .header {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  width: 100%;
+  padding: 16px;
+}
+.login-screen .body {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  width: 80%;
+  margin: 0 auto;
+  flex-grow: 1;
+}
+</style>
---
+++ src/views/Main.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="main-screen">
+    <div>
+      <i class="fa fa-4x fa-car"></i>
+            <h1>CUBER</h1>
+    </div>
+
+    <div style="width: 80%">
+      <router-link class="btn btn-block" style="background-color: white" to="/login">
+      Se connecter
+      </router-link>
+      <router-link class="btn btn-block btn-primary" to="/register">
+      Créer un compte
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.main-screen {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  flex-direction: column;
+  height: 100vh;
+}
+</style>
